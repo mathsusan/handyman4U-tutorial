050d25b7c9b28c3b45b30d1b080e075f390496ca
Susan McKenzie <8696929+mathsusan@users.noreply.github.com>
2024-06-02 11:25:01 -0500

    Delete handyman directory

--- handyman/scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleFunction() {
-  var header = document.getElementById('main-header');
-  if (header.classList.contains('open')) {
-    header.classList.remove('open');
-  } else {
-    header.classList.add('open');
-  }
-}
